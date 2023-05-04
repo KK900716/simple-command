a0135b8d67967c3078b7db8d9a951635ea0383c5
Liangjc <443808626@qq.com>
2023-05-04 09:27:49 +0800

    fix:依赖

--- src/main/java/com/example/sync/manager/controller/executor/ExecutorServiceFactory.java
+++ src/main/java/com/example/sync/manager/controller/executor/ExecutorServiceFactory.java
@@ -1,13 +1,13 @@
 package com.example.sync.manager.controller.executor;
 
 import java.util.concurrent.ThreadFactory;
-import org.jetbrains.annotations.NotNull;
+import javax.annotation.Nonnull;
 
 public class ExecutorServiceFactory implements ThreadFactory {
   private static int COUNTER = 1;
 
   @Override
-  public Thread newThread(@NotNull Runnable r) {
+  public Thread newThread(@Nonnull Runnable r) {
     return new Thread(r, "Service-" + COUNTER++);
   }
 }
